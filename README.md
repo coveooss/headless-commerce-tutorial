bae06a942762d3f42331106af7580f07248225d4
Sym Piracha <spiracha@coveo.com>
2023-03-15 14:58:24 -0400

    add starter code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# headless-commerce-tutorial
+# Coveo Headless Commerce Starter Project
+
+This project serves as a starting point for the [XXXX](https://levelup.coveo.com/learn/courses/headless-tutorial).
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Setup
+
+Clone or download the project and run `npm install` in the project directory.
+
+## `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { loadSearchActions, loadSearchAnalyticsActions } from '@coveo/headless';
+import './App.css';
+import ResultList from './components/ResultList';
+import SearchBox from './components/SearchBox';
+import Facet from './components/Facet';
+import { 
+  searchBox as SearchBoxController,
+  resultList as ResultListController,
+  facet as FacetController
+   } from './controllers/controllers';
+import { headlessEngine } from './Engine';
+function App() {
+  
+  useEffect(() => {
+    const {logInterfaceLoad} = loadSearchAnalyticsActions(headlessEngine);
+    const {executeSearch} = loadSearchActions(headlessEngine);
+    headlessEngine.dispatch(executeSearch(logInterfaceLoad()));
+  },[]);
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={require('./assets/barca.svg').default} alt='barcaLogo' />
+        <div className="search-section">
+          <SearchBox controller={SearchBoxController}/>
+        </div>
+      </header>
+      <div className="app-body">
+        <div className="main-section">
+          <div className="facet-section column">
+            <Facet controller={FacetController} title="Category" />
+          </div>
+          <div className="results-section column">
+            <ResultList controller={ResultListController}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
