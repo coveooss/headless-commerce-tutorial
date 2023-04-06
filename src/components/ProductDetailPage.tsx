76c5d649a9f8ede248c3ccab8b5c409f8bc083c9
Sym Piracha <spiracha@coveo.com>
2023-04-06 15:30:47 -0400

    lint

--- src/components/ProductDetailPage.tsx
+++ src/components/ProductDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useLocation,useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { FreqViewedTogether } from "./Recommendations/FreqViewedTogether";
 import CartRecommendations from "./Recommendations/CartRecommendations";
 import {
