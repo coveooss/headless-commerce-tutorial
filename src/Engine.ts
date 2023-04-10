d0d158bc36c0c7c4529d27fe4a242e1681afd96b
Sym Piracha <spiracha@coveo.com>
2023-04-10 09:27:27 -0400

    add search hub

--- src/Engine.ts
+++ src/Engine.ts
@@ -62,6 +62,7 @@ const createPREngine = () =>
     configuration: {
       organizationId: "barcagroupproductionkwvdy6lp",
       accessToken: "xx5a7943ef-ea52-42e5-8742-51198cc651f7",
+      searchHub: 'PDP',
     },
   });
 
