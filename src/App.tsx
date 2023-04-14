ba3557d9ae38040653983370a77909cc8bfa2c4b
Sym Piracha <spiracha@coveo.com>
2023-04-14 14:35:17 -0400

    update view event

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ declare global {
 
 function App() {
   const logViewEvent = () => {
-    coveoua("set", "page", "/");
+    coveoua("set", "page", window.location.pathname)
     coveoua("send", "pageview");
   };
 
